c768b421ad719fad4a439de2d304502e50111e90
Nchedochukwu Bede <nchedo017.com>
2022-09-09 10:22:05 -0700

    Check for number sign code.add\n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,6 @@
 
 /**
  * main - Checks state of number
- * @n: number to be checked
  *
  * Description: Checks if n is positive or negative
  * Return: Always 0
@@ -16,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	else
-		printf("%d is negative", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
