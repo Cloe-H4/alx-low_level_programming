6c5763418bcce797467864f673a7f3da0f949629
Nchedochukwu Bede <nchedo017.com>
2022-09-09 11:27:08 -0700

    Use putchar test1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints all lowercase alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+
+	a = "abcdefghijklmnopqrstuvwxyz ";
+	putchar(a);
+	return (0);
+}
