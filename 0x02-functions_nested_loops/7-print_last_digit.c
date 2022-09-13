ac85c06bef8b58fc734129bac62cc04a2d836aa3
Nchedochukwu Bede <nchedo017.com>
2022-09-13 12:46:09 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of n
+ * @n: Inputted number
+ *
+ * Return: 0 as success
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	return (n % 10);
+	return (0);
+}
