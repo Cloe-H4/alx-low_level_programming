df4ed62ab7131e5f760192cc8561613806fc3d2d
Nchedochukwu Bede <nchedo017.com>
2022-09-13 10:26:59 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints lowercase 10 times
+ * 
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i=0;
+
+	while (i < 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+	i++
+	_putchar('\n')
+	}
+}
