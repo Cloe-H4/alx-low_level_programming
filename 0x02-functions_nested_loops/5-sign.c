08635ee5f90554ee9dfd4aaa543c7f5f91bdbbd3
Nchedochukwu Bede <nchedo017.com>
2022-09-13 12:09:32 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of number
+ * @n: Inputted number
+ *
+ * Return: 1 for positive, 0 for zero
+ * and -1 for negative.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+}
