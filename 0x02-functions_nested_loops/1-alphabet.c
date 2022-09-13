0f19c9d0dad575d543cede682bb6af37d7b0a3f5
Nchedochukwu Bede <nchedo017.com>
2022-09-13 08:25:50 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Print a-z
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,2 @@
-#include <stdio.h>
-
-void _putchar(char i)
-{
-	putchar(i);
-}
+int _putchar(char c);
+void print_alphabet(void);
