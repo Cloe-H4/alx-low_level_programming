c26a9d8c2fb16f57c0a7c0a791d768ac5c45eb14
Nchedochukwu Bede <nchedo017.com>
2022-09-13 14:03:57 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the timestables from 0 to 9
+ *
+ * Return: 0 as success
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else
+			{
+				_putchar(c + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
