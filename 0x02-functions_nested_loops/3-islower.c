498172315fbf3d250a8e62ba95967bd890e7eb73
Nchedochukwu Bede <nchedo017.com>
2022-09-13 11:27:12 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Checks letter case
+ *
+ * Return: 1 if lower, 0 if Upper
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+	
