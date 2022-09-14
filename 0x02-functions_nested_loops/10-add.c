35a3bcb37656de703c2231c253864c1c64f76ea5
Nchedochukwu Bede <nchedo017.com>
2022-09-13 23:46:12 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - sums two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
