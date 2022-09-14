3fc2ae37a4e2dd319f042e3307b81c32d9644f0d
Nchedochukwu Bede <nchedo017.com>
2022-09-14 01:15:34 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the timestable from 0 to n
+ * @n: number given
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int a, w, z;
+
+	for (w = 0; w <= n; w++)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			z = w * a;
+			if (a == 0)
+				printf("%d", a);
+			else if (z < 10)
+				printf(",   %d", z);
+			else if ((z > 9) && (z < 100))
+				printf(",  %d", z);
+			else if (z > 99)
+				printf(", %d", z);
+		}
+		_putchar('\n');
+	}
+}
