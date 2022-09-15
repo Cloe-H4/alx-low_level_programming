a94dfe02064b288a9ad0c485a5273e870a6df681
Nchedochukwu Bede <nchedo017.com>
2022-09-15 10:18:08 -0700

    Initial commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Prints the n number of _
+ * @n: number input
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 1; a <= n; a++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
