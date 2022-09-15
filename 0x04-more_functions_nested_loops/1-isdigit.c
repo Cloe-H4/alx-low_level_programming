c72909be95c3543b9ae926ca712525e8ba22f9a9
Nchedochukwu Bede <nchedo017.com>
2022-09-15 08:22:28 -0700

    Initial commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks if c is a number between 0-9
+ * @c: inputted character
+ *
+ * Return: 1 if c is number and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
