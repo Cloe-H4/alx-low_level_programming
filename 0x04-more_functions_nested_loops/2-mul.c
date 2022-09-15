e5202781d99d2b7ebdde7fc0df5f046d9a3c027a
Nchedochukwu Bede <nchedo017.com>
2022-09-15 08:34:38 -0700

    Initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Calculates products of two numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
