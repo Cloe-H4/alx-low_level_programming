42f4fbf278e4235f0fffe05096d317aaadcc2399
Nchedochukwu Bede <nchedo017.com>
2022-09-15 09:01:55 -0700

    Initial commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
