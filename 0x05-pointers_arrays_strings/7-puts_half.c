0ae2ccdb766f1bdc43700d522f75c3c6a0b6e3d1
Nchedochukwu Bede <nchedo017.com>
2022-09-19 11:10:53 -0700

    Try code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 	int i, even, neg;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
+		;
 		if ((i % 2) == 0)
 		{
 			for (even = i / 2; str[even] != '\0'; even++)
@@ -23,6 +23,5 @@ void puts_half(char *str)
 			for (neg = (i - 1) / 2; str[neg] != '\0'; neg++)
 				_putchar(str[neg + 1]);
 		}
-	}
 	_putchar('\n');
 }
