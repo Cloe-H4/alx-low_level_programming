1b8f0ecd14e8489f0fed987336876432512ea41f
Nchedochukwu Bede <nchedo017.com>
2022-09-22 11:41:19 -0700

    Initial commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - it reverses the array elements
+ * @a: array inputted
+ * @n: number of array elements
+ *
+ * Return: Array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n / 2; i++, n--)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
+	}
+	return (a);
+}
