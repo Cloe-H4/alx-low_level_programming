d727d36c38d1c25072319f6d6421ab706136f3a6
Nchedochukwu Bede <nchedo017.com>
2022-09-22 08:03:53 -0700

    Afford mistake

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int s;
 
-	for (s = 0; s < n; s++)
-	{
-		if (src[s] != '\0')
-			dest[s] = src[s];
-		else
-			dest[s] = '\0';
-	}
+	for (s = 0; s < n && src[s] != '\0'; s++)
+		dest[s] = src[s];
+
+	for (; s < n; s++)
+		dest[s] = '\0';
+
 	return (dest);
 }
