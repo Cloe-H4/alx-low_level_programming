af003b984ecb9ff3bc7198faf27cac64a8d629ef
Nchedochukwu Bede <nchedo017.com>
2022-09-28 14:49:40 -0700

    Add betty rules

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,8 +2,9 @@
 
 int findsq(int n, int root);
 /**
- * _sqrt_recursion - function return square root of n
+ * findsq - function return square root of n
  * @n: inputted number
+ * @root: the square root
  *
  * Return: square root
  */
@@ -17,6 +18,13 @@ int findsq(int n, int root)
 	else
 		return (findsq(n, root + 1));
 }
+
+/**
+ * _sqrt_recursion - finds the square root of n
+ * @n: number to find square root
+ *
+ * Return: the square root
+ */
 
 int _sqrt_recursion(int n)
 {
