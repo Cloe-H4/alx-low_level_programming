439e506ef35e7bf9730eacec1734d904d11816f4
Nchedochukwu Bede <nchedo017.com>
2022-09-28 09:16:05 -0700

    Submit work

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints the reverse of string using recursion
+ * @s: inputted string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s[1] == '\0')
+	{
+		_putchar(s[0]);
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[0]);
+	}
+}
