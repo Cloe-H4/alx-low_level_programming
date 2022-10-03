01f7a0cb2074058022c8bbf89f94e6fd42a95573
Nchedochukwu Bede <nchedo017.com>
2022-10-03 13:10:32 -0700

    Submit work

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - creates a duplicate of string in heap.
+ * @str: inputted string
+ *
+ * Return: NULL if str is empty or insufficient space in heap
+ * and pointer to string duplicate if successful.
+ */
+
+char *_strdup(char *str)
+{
+	int len;
+	int count = 0;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	dup = malloc(len);
+	if (dup == NULL)
+		return (NULL);
+
+	while (count < len)
+	{
+		dup[count] = str[count];
+		count++;
+	}
+	dup[len] = '\0';
+	return (dup);
+}
