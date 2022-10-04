340e286b7f98cdc42ffb02e1506a156fede60a06
Nchedochukwu Bede <nchedo017.com>
2022-10-04 07:24:34 -0700

    Modify string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,11 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2;
 	char *join;
 
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+
 	len1 = 0;
 	while (s1[len1])
 		len1++;
