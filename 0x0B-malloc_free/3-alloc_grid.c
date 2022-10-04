45629f18e7debcc693fa069693487f1d3024170e
Nchedochukwu Bede <nchedo017.com>
2022-10-04 10:06:00 -0700

    Submit work

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a two dimensional array of intergers
+ * @width: the size of the array
+ * @height: number of arrays
+ *
+ * Return: NULL for error and arrays for success
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, w, h;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(void *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (h = 0; h < height; h++)
+	{
+		grid[h] = malloc(sizeof(int) * width);
+		if (grid[h] == NULL)
+			return (NULL);
+		w = 0;
+		while (w < width)
+		{
+			grid[h][w] = 0;
+			w++;
+		}
+	}
+	return (grid);
+}
