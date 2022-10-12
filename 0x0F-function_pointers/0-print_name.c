a6519eba7a532ea4a26228f7b361db6080df536e
Nchedochukwu Bede <nchedo017.com>
2022-10-12 10:49:13 -0700

    Put check for parameters

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,13 @@
 
 /**
  * print_name - prints name in wanted format.
- * @name - the inputted name
- * @f - the function that creates the wanted format.
+ * @name: the inputted name
+ * @f: the function that creates the wanted format.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL || name == NULL)
+		return;
 	f(name);
 }
