296e07e20003e11d3e563ee1200e3294992951f9
Nchedochukwu Bede <nchedo017.com>
2022-10-26 13:06:31 -0700

    Submit work

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,9 +8,16 @@
 
 void free_listint2(listint_t **head)
 {
-	if (*head == NULL)
-		return ;
-	free_listint2(*head->next);
-	free(*head);
-	return;
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	head = NULL;
 }
